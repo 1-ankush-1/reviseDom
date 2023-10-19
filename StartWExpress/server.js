8586ac477300f8de93ca05bedb04f13aa0a8d947
Ankush <ankushsharma9685@gmail.com>
2023-10-19 17:31:24 +0530

    Intro to middleware complete

--- StartWExpress/server.js
+++ StartWExpress/server.js
@@ -1,14 +1,46 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+
+//initalise new exprees object
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("using middleware");
-    next();
+//body-parser do the req body parsing(handel form body not files and json)
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    }),
+  );
+
+//add product route
+app.use("/add-product", (req, res, next) => {
+    res.send(`
+    <html>
+        <head>
+            <title>Product</title>
+        </head>
+        <body>
+            <form action="/product" method="POST">
+                <input type="text" name="name">
+                <input type="number" name="size">
+                <button type="submit">Submit</button>
+            </form>
+        </body>
+    </html>
+    `);
 })
 
-app.use((req, res, next) => {
-    console.log("showing ");
-    res.send("<h1>Hello</h1>");
+//handel post request and redirect to home
+app.post("/product", (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/");
 })
 
-app.listen(3000);
+//root route
+app.use("/", (req, res, next) => {
+    res.send("HomePage");  //send response of anykind
+})
+
+//start the server
+app.listen(4000, () => {
+    console.log(`server is running on http://localhost:${4000} `);
+})
