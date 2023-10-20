001335af6736eea26f881c3de92ecb57e43e2fbe
Ankush <ankushsharma9685@gmail.com>
2023-10-20 19:09:57 +0530

    added relation of order and cart

--- Ecommerce-APP/controllers/shop.js
+++ Ecommerce-APP/controllers/shop.js
@@ -28,7 +28,6 @@ exports.getIndex = (req, res, next) => {
     console.log(`${err} in getIndex`)
   });
 };
-
 exports.getCart = (req, res, next) => {
   req.user.getCart().then(cart => {
     return cart.getProducts().then(products => {
@@ -39,9 +38,7 @@ exports.getCart = (req, res, next) => {
       });
     }).catch(err => console.log(err));
   }).catch(err => console.log(err));
-
 };
-
 exports.postCart = (req, res, next) => {
   const { productId: prodId } = req.body
   let fetchedCart;
@@ -65,22 +62,45 @@ exports.postCart = (req, res, next) => {
   }).then(() => {
     res.redirect('/cart')
   }).catch(err => console.log(err));
-
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+
+  req.user.getOrders({include:['products']}).then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  }).catch(err => console.log(err));
+
 };
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user.getCart().then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  }).then(products => {
+    return req.user.createOrder().then(order => {
+      return order.addProducts(products.map(product => {
+        product.orderItem = { quantity: product.cartItem.quantity }
+        return product;
+      }))
+    }).catch(err => console.log(err));
+  }).then(result => {
+    return fetchedCart.setProducts(null);
+  }).then(result => {
+    res.redirect("/orders");
+  }).catch(err => console.log(err));
+}
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
   });
 };
-
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user.getCart()
