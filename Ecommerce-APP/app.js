e28e38095bd0b0589c7d16711f12e907f1773847
Ankush <ankushsharma9685@gmail.com>
2023-10-20 19:01:49 +0530

    added relation with cart

--- Ecommerce-APP/app.js
+++ Ecommerce-APP/app.js
@@ -5,15 +5,17 @@ const errorController = require('./controllers/error');
 const sequelize = require("./util/database.js")
 const Product = require('./models/product')
 const User = require('./models/user');
-
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 //registering the user
 app.use((req, res, next) => {
     User.findByPk(1).then(user => {
@@ -21,14 +23,16 @@ app.use((req, res, next) => {
         next();
     }).catch(err => console.log(err));
 })
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
 
 sequelize.sync().then(result => {
     return User.findByPk(1)
@@ -41,7 +45,8 @@ sequelize.sync().then(result => {
     }
     return user;
 }).then(user => {
-    // console.log(user);
+    return user.createCart();
+}).then(cart=>{
     app.listen(3000);
 }).catch(err => {
     console.log(`${err} occured whne syncing with sequalize`)
